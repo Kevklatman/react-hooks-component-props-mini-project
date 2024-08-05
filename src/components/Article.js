6f4df71684764c058562dd4c8e3df13bf63dc1a7
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-05 01:16:05 -0600

    lab complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 function Article({ title, date, preview, minutes }) {
-  const minutesToRead = (minutes) => {
-    const cups = Math.ceil(minutes / 5);
-    return "☕️".repeat(cups) + ` ${minutes} min read`;
-  };
-
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date || "January 1, 1970"} - {minutesToRead(minutes)}</small>
+      <small>{date || "January 1, 1970"}</small>
       <p>{preview}</p>
     </article>
   );
